557bf94ca2d986181cd7da3b2179c8742b7e085d
woooyoung <axdsw121@gmail.com>
2024-04-11 16:24:58 +0900

    메세지를 리스트에 저장

--- src/main/java/com/koreait/exam/chat_app_24_04/ChatController.java
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatController.java
@@ -3,14 +3,33 @@ package com.koreait.exam.chat_app_24_04;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
-    ChatMessage message = new ChatMessage("홍길동","안녕하세요");
-    
-    @PostMapping ("/writeMessage")
-    @ResponseBody
-    public RsData<ChatMessage> writeMessage() {
-        return new RsData("S-1","메세지가 작성됨",message);
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
+    public record writeMessageResponse(long id) {
+
     }
+
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public RsData<writeMessageResponse> writeMessage() {
+
+        ChatMessage message = new ChatMessage("홍길동", "안녕하세요");
+
+        chatMessages.add(message);
+
+        return new RsData(
+                "S-1",
+                "메세지가 작성됨",
+                new writeMessageResponse(message.getId()
+                )
+        );
+    }
+
+
 }
